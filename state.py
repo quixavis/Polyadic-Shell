d9d9516a8ba5d86040c60ba845de80d63a0d48d0
quixavis <nightmarev06@gmail.com>
2025-11-24 20:10:02 -0500

    Add files via upload

---
+++ state.py
@@ -0,0 +1,11 @@
+import time
+
+game_state = {
+    "coins": 0,
+    "production_rate": 0,
+    "generators": 0, 
+    "gen_cost": 0,
+    "prestiges": 0,
+    "pres_cost": 0,
+    "last_time": time.time()
+}
